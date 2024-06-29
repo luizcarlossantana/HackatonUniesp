e4dcc3f2c4f824344b3d011152d0f065675982ab
IAbrahanArley <abrahanarley96@gmail.com>
2024-06-29 14:14:06 -0300

    Feat: sistema de login

--- api_receita/API/src/main/java/br/com/receitafederal/API/repository/UsuarioRepository.java
+++ api_receita/API/src/main/java/br/com/receitafederal/API/repository/UsuarioRepository.java
@@ -5,12 +5,15 @@ import br.com.receitafederal.API.Model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByUsuarioname(String username);
-    Usuario findByCpf(String cpf);
+    Optional<Usuario> findByUsuarioname(String username);
+    Optional<Usuario> findByCpf(String cpf);
+    Optional<Usuario> findByUsuarionameAndEmail(String username, String email);
+    Optional<Usuario> findByEmail(String email);
 
-    Usuario findByUsuarionameAndEmail(String username, String email);
 
 
 }
