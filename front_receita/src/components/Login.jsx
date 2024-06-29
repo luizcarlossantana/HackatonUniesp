294131ceb428f5b3da47a065175ec1c5f1a92b98
Luiz Carlos Santana <112254950+luizcarlossantana@users.noreply.github.com>
2024-06-29 17:27:16 -0300

    front

--- front_receita/src/components/Login.jsx
+++ front_receita/src/components/Login.jsx
@@ -1,34 +1,39 @@
-import styles from './Login.module.css'; // Importe os estilos do módulo CSS
+import React from 'react';
 
 const Login = () => {
   return (
-  <div className={styles.geral}>  
-      <div className={styles.container}>
-      <div className={styles.leftSection}>
-        <div className={styles.logo}>
-          <img src="./assets/logoReceitaWhite.svg" alt="logo da Receita Federal" />
+    <div className="container pt-5 pb-5"> {/* Adicionando margem superior (mt-5) e inferior (mb-5) */}
+      <div className="d-flex">
+        <div className="col-md-6 gradient-background  pt-5 pb-5">
+          <div className="logo d-flex" style={{ justifyContent: 'center', alignItems: 'center'}}>
+            <img src='logoReceitaWhite.svg' alt="logo da Receita Federal" className="img-fluid" style={{ width: '400px', height: 'auto' }}/>
+          </div>
+        </div>
+        <div className="col-md-6  pt-5 pb-5">
+          <div className="right-screen1  pt-5 pb-5 d-flex flex-column justify-content-center align-items-center">
+            <h2>Entrar</h2>
+
+            <form action="/submit" method="post">
+              <div className="mb-3">
+                <label htmlFor="text" className="form-label">Login</label>
+                <input type="text" className="form-control" id="text" name="text" placeholder="nome, cpf, email" required />
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">Senha</label>
+                <input type="password" className="form-control" id="password" name="password" required />
+              </div>
+
+              <p>
+                Ainda não tem conta? <a href="./tela2.html">Criar conta</a>
+              </p>
+
+              <button type="button" className="btn btn-primary">Fazer Login</button>
+            </form>
+          </div>
         </div>
       </div>
-
-      <div className={styles.rightScreen1}>
-        <h2>Entrar</h2>
-
-        <form action="/submit" method="post">
-          <label htmlFor="text">Login</label>
-          <input type="text" id="text" name="text" placeholder="nome, cpf, email" required />
-
-          <label htmlFor="password">Senha</label>
-          <input type="password" id="password" name="password" required />
-
-          <p>
-            Ainda não tem conta? <a href="./tela2.html">Criar conta</a>
-          </p>
-
-          <button type="button">Fazer Login</button>
-        </form>
-      </div>
     </div>
-  </div>
   );
 }
 
